45d97f402641ab2269f0e3f9c02ea387736266ca
SebaZ-jpg <sebastianalonsoaceroguiop@gmail.com>
2026-03-12 11:14:35 +0100

    proyecto final 2.1

--- Gestion_Peliculas/src/main/java/dam/code/service/PeliculaService.java
+++ Gestion_Peliculas/src/main/java/dam/code/service/PeliculaService.java
@@ -29,14 +29,31 @@ public class PeliculaService {
 
     private void validarPelicula(Pelicula pelicula) throws PeliculasException {
 
+        if (pelicula.getId() == null || pelicula.getId().isBlank()) {
+            throw new PeliculasException("El titulo es obligatorio");
+        }
+
         if(pelicula.getTitulo() == null || pelicula.getId().isBlank()) {
             throw new PeliculasException("El titulo es obligatorio");
 
         }
 
-        if (pelicula.getDirector() == null || pelicula.getId().isBlank()) {
+        if (pelicula.getDirector() == null || pelicula.getDirector().isBlank()) {
             throw new PeliculasException("El director es obligatorio");
         }
+
+        if (pelicula.getDuracion() == null || pelicula.getDuracion() {
+            throw new PeliculasException("El duracion es obligatoria");
+        }
+
+        if (pelicula.getFecha_publicacion() == null || pelicula.getFecha_publicacion().isBlank()) {
+            throw new PeliculasException("La fecha publicacion es obligatoria");
+        }
+
+        private void guardar() {
+            repository.guardar(peliculas);
+        }
+
 
     }
 
